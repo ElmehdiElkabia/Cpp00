84f48adb4729de4cd758dce6ca2d97d50c72008e
elmehdi elkabia <elmehdielkabia@gmail.com>
2025-07-11 16:18:17 +0100

    update the name space for std::

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: eelkabia <eelkabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 15:46:42 by eelkabia          #+#    #+#             */
-/*   Updated: 2025/07/11 16:06:06 by eelkabia         ###   ########.fr       */
+/*   Updated: 2025/07/11 16:16:13 by eelkabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 
 	i = 1;
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else
 	{
 		while (argv[i])
@@ -30,12 +30,12 @@ int main(int argc, char **argv)
 			{
 				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
 					argv[i][j] -= 32;
-				std::cout << argv[i][j];
+				cout << argv[i][j];
 				j++;
 			}
 			i++;
 		}
-		std::cout << std::endl;
+		cout << endl;
 	}
 	return (0);
 }
